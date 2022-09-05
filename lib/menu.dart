d2cad855232e0d3bfa344fc57448784dff3f2fa1
Shuttoku <108716387+Shuttoku@users.noreply.github.com>
2022-09-05 18:18:18 +0700

    ooook

--- lib/menu.dart
+++ lib/menu.dart
@@ -2,7 +2,6 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
-
 class Menu extends StatefulWidget {
   const Menu({Key? key}) : super(key: key);
 
@@ -12,25 +11,33 @@ class Menu extends StatefulWidget {
 
 class _MenuState extends State<Menu> {
   List<List<dynamic>> foodLists = [
-
-["https://www.amarinbabyandkids.com/app/uploads/2022/05/%E0%B9%84%E0%B8%82%E0%B9%88%E0%B9%80%E0%B8%88%E0%B8%B5%E0%B8%A2%E0%B8%A7FB.jpg",
-"ไข่เจียวหาดใหญ่",
-80],
-["https://food.mthai.com/app/uploads/2014/12/iStock-494899370.jpg","ไข่กระทะฮาวาย",90],
-["https://images.aws.nestle.recipes/resized/12f17c7de982b57fdf3e4c9091cd3b29_steamed-egg-with-minced-pork_944_531.jpeg","ไข่ตุ๋นคุณหนู",70],
-["http://www.pim.in.th/images/all-side-dish-egg/egg-soup-with-mushroom/106.JPG","ไข่น้ำทะเล",80],
+    [
+      "https://www.amarinbabyandkids.com/app/uploads/2022/05/%E0%B9%84%E0%B8%82%E0%B9%88%E0%B9%80%E0%B8%88%E0%B8%B5%E0%B8%A2%E0%B8%A7FB.jpg",
+      "ไข่เจียวหาดใหญ่",
+      80
+    ],
+    [
+      "https://food.mthai.com/app/uploads/2014/12/iStock-494899370.jpg",
+      "ไข่กระทะฮาวาย",
+      90
+    ],
+    [
+      "https://images.aws.nestle.recipes/resized/12f17c7de982b57fdf3e4c9091cd3b29_steamed-egg-with-minced-pork_944_531.jpeg",
+      "ไข่ตุ๋นคุณหนู",
+      70
+    ],
+    [
+      "http://www.pim.in.th/images/all-side-dish-egg/egg-soup-with-mushroom/106.JPG",
+      "ไข่น้ำทะเล",
+      80
+    ],
   ];
-
-
-
-
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: foodLists.length,
-      itemBuilder:(_, index)
-      {
+      itemBuilder: (_, index) {
         return Card(
           // elevation: 0,
           shape: RoundedRectangleBorder(
@@ -43,17 +50,16 @@ class _MenuState extends State<Menu> {
               children: [
                 Row(
                   children: [
-                  Image.network(foodLists[index][0]),
+                    Image.network(foodLists[index][0],
+                    fit: BoxFit.cover,
+                    ),
                   ],
-                  ),
-
+                ),
               ],
             ),
           ),
         );
-
       },
     );
-    
   }
 }
